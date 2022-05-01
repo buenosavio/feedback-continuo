060e786bf157390cac112f4f99b07051dc3a4be0
César Fernandes Luchesi <cesarfluchesi123@gmail.com>
2022-05-01 10:16:46 -0300

    card styled

--- feedback-continuo/src/pages/home/Home.styles.tsx
+++ feedback-continuo/src/pages/home/Home.styles.tsx
@@ -9,7 +9,8 @@ min-width: 100vh;
 min-height:50vh;
 max-height: 1200px;
 max-width: 1200px;
-
+box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
+  border-radius: 8px;
 `;
 
 export const Pagination = styled.button`
