8a061a117b6e2de3ca15f812caa1e04747528067
César Fernandes Luchesi <cesarfluchesi123@gmail.com>
2022-05-01 17:45:35 -0300

    style home

--- feedback-continuo/src/components/tabs/TabTitle.tsx
+++ feedback-continuo/src/components/tabs/TabTitle.tsx
@@ -8,21 +8,15 @@ type Props = {
   index: number
   setSelectedTab: (index: number) => void
 }
- 
-
 const TabTitle: FC<Props> = ({title, setSelectedTab, index}) => {
-
-
   const onClick = useCallback(() => {
     setSelectedTab(index)
   }, [setSelectedTab, index])
-
   return (
     <LiTab>
       <ButtonTab setSelectedTab='is-selected' onClick={onClick}>{title}</ButtonTab>
     </LiTab>
   )
 }
-
 export default TabTitle
   
