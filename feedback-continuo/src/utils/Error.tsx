d8b60d350321df6b429cead198a8da0ad6e254f9
Savio B <souzasavio@outlook.com>
2022-05-01 23:01:54 -0300

    feat: ajustes finais

--- feedback-continuo/src/utils/Error.tsx
+++ feedback-continuo/src/utils/Error.tsx
@@ -3,40 +3,46 @@ import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const handleError = (errorData: AxiosError) => {   
   if (errorData.response) {
-    console.log(errorData.response.data.message)
     switch (errorData.response.data.message) {
       case 'Não é possível atribuir feedbacks a si mesmo.':
         Notify.failure('Não é possível atribuir feedbacks a você mesmo.');
         break;
       case 'Email inválido ou já existente.':
         Notify.failure('E-mail já existente. Tente novamente!');
-      break;
+        break;
       case 'Senha fraca demais.':
         Notify.failure('Senha Fraca. Tente novamente!'); 
-      break;
+        break;
       case 'Senha antiga incompatível.':
         Notify.failure('Senha anterior não confere.');
-      break;
+        break;
       case 'Essa já é sua senha atual!':
         Notify.failure('Nova senha deve ser diferente da anterior. Tente novamente!');
-      break;
+        break;
       case 'Tipo de arquivo não suportado.':
         Notify.warning('Tipo de arquivo não suportado! Insira JPEG, JPG ou PNG');
-      break;
+        break;
       case 'Email inválido.':
         Notify.warning('Email inválido. Tente novamente!');
-      break;
+        break;
       case 'Usuário não encontrado.': 
         Notify.failure('Usuário não encontrado.')
         break;
       case 'Erro ao salvar imagem.':
         Notify.failure('Erro ao salvar imagem. Tente novamente!')
+        break;    
+      case undefined:        
         break;
       default:
         Notify.failure('Erro desconhecido. Tente novamente!');
-    }      
+    } 
+    switch(errorData.response.status) {
+      case 403:
+        Notify.failure('Login inválido. Tente novamente!')
+        break;
+    }
   } else {
-    Notify.failure('Erro desconhecido. Tente novamente!'); 
+    Notify.failure('Errosss desconhecido. Tente novamente!'); 
   }     
 }
 
